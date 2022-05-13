b4938e252ceb7c056f37582b154d3c3bae420203
Fatima Abrar <abrarfatima89@gamil.com>
2022-05-13 00:06:59 -0400

    one get endpoint working

--- server.js
+++ server.js
@@ -7,9 +7,17 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const moodboardsDB = './data/moodboards.json'
 
-app.use(espress.json());
+app.use(express.json());
 app.use(cors())
 
+function accessData () {
+    return JSON.parse(fs.readFileSync(moodboardsDB))
+}
+
+app.get('/mood-boards', (req,res) => {
+    const pullData = accessData()
+    return res.json(pullData)
+})
 
 app.listen(PORT, ()=> {
     console.log("Server is listening at port: ", PORT)
