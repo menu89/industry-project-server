0642c77742f01fbcd3f3ae3c630e6bb4708f5280
Fatima Abrar <abrarfatima89@gamil.com>
2022-05-13 00:17:02 -0400

    update json data and added comments to server.js

--- server.js
+++ server.js
@@ -7,18 +7,24 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const moodboardsDB = './data/moodboards.json'
 
-app.use(express.json());
+//middleware
 app.use(cors())
+app.use(express.json());
+app.use(express.static('public'))
 
+//getdata from the json files
 function accessData () {
     return JSON.parse(fs.readFileSync(moodboardsDB))
 }
 
+//endpoint search
 app.get('/mood-boards', (req,res) => {
     const pullData = accessData()
     return res.json(pullData)
 })
 
+
+//server setting
 app.listen(PORT, ()=> {
     console.log("Server is listening at port: ", PORT)
 })
